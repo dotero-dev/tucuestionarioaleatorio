daee788664110f020e17c5f16ffbf374f650ad0e
dotero <doterodev@gmail.com>
2024-02-04 10:08:37 +0100

    Prueba.js eliminado

--- prueba.js
+++
@@ -1,7 +0,0 @@
-var pdfUtil = import('pdf-to-text');
-var pdf_path = "RES_FYQ_3ESO_23-24.pdf";
- 
-pdfUtil.info(pdf_path, function(err, info) {
-    if (err) throw(err);
-    console.log(info);
-});
